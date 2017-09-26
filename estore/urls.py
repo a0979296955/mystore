e0d47794ada65390a8ee0a88acf5fa4f96edd794
a0979296955 <a0979296955@example.com>
2017-09-27 01:29:58 +0800

    Create backstage

--- estore/urls.py
+++ estore/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include,url
-from django.contrib import admin
+from django.conf.urls import url
+
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-	url(r'', include('estore.urls')),
+    url(r'^$', views.product_list, name='product_list'),
+    url(r'^create$', views.product_create, name='product_create'),
 ]
